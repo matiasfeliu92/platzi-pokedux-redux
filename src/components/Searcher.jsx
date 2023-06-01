366b863cbde8ee75931b3a0894be07da45099c3e
matiasfeliu92 <matumazparrote@gmail.com>
2023-06-01 19:31:50 -0300

    refactor: add handleSearch function to change search state

--- src/components/Searcher.jsx
+++ src/components/Searcher.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import { Input } from "antd";
 
-const Searcher = () => {
+const Searcher = ({search, setSearch}) => {
+  console.log(search)
+
+  const handleSearch = (e) => {
+    const searchText = e.target.value
+    setSearch(searchText)
+  }
+
   return (
-    <Input.Search placeholder='buscar'/>
+    <Input.Search placeholder='search by type' onChange={handleSearch}/>
   )
 }
 
