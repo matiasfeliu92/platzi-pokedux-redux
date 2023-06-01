1651c067ef3389dae4ea98f8c5a7a2b22cb690f6
matiasfeliu92 <matumazparrote@gmail.com>
2023-06-01 19:31:10 -0300

    refactor: add filter function in PokemonList, to filter pokemons if the searchbar has something entered

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,7 +1,27 @@
 import React from 'react'
 import PokemonCard from './PokemonCard'
 
-const PokemonList = ({ pokemons }) => {
+const PokemonList = ({ pokemons, search }) => {
+  console.log(search)
+  let pokemonsFiltered
+  if(search != ""){
+    pokemonsFiltered = pokemons.filter((pok) => pok.types[0].type.name.includes(search))
+    return (
+      <div className='PokemonList'>
+        {pokemonsFiltered.map((pokemon) => (
+          <PokemonCard
+            key={pokemon.id}
+            name={pokemon.name}
+            image={pokemon.sprites.front_default}
+            habilities={pokemon.abilities}
+            types={pokemon.types}
+            id={pokemon.id}
+            favorite={pokemon.favorite} // Agrega la propiedad 'favorite' al componente PokemonCard
+          />
+        ))}
+      </div>
+    )
+  }
   return (
     <div className='PokemonList'>
       {pokemons.map((pokemon) => (
